0f51e851cc4c77a0d0639eadfc903c4a4a707393
Daniele Tricoli <eriol@mornie.org>
2009-10-27 03:34:14 +0100

    Using a meta model to link other flatpages

--- microcms/models.py
+++ microcms/models.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.conf import settings
+from django.contrib.flatpages.models import FlatPage
 
-# Create your models here.
+class Meta(models.Model):
+    flatpage = models.OneToOneField(FlatPage)
+    links = models.ManyToManyField(FlatPage, related_name='superpages')
+
+    def __unicode__(self):
+        return self.flatpage.title
