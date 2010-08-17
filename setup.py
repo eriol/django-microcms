fab485bbc4fd6e756dff2212e3c8ade6fcf2ac41
Daniele Tricoli <eriol@mornie.org>
2010-08-18 00:01:42 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+from microcms import VERSION
+
+setup(name='microcms',
+      version=VERSION,
+      description='Simple flatpage enhancement',
+      author='Daniele Tricoli',
+      author_email='eriol@mornie.org',
+      package_dir={'microcms': 'microcms'},
+      packages=['microcms']
+     )
