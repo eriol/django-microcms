a0e4d490d52da714fc52c534abfc99fa0ca35944
Daniele Tricoli <eriol@mornie.org>
2010-11-16 00:01:56 +0100

    Added help text

--- microcms/models.py
+++ microcms/models.py
@@ -9,16 +9,27 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Page(FlatPage):
-    links = models.ManyToManyField('Page', related_name='superpages',
-                                   blank=True, null=True)
     author = models.ForeignKey(User, verbose_name=_('author'))
     pub_date = models.DateTimeField(_('publication date'), auto_now_add=True)
-    modified_date = models.DateTimeField(_('last update date'), auto_now=True)
+    modified_date = models.DateTimeField(_('last modified date'),
+                                         auto_now=True)
+    links = models.ManyToManyField('Page', blank=True, null=True,
+                                   related_name='superpages',
+                                   verbose_name=_('page'))
     # Fields for Search Engine Optimization
     meta_keywords = models.CharField(_('meta keywords'), blank=True,
+                                     help_text=_('Key words of the page. '
+                                                 'Max 255 characters.'),
                                      max_length=255)
     meta_description = models.CharField(_('meta description'), blank=True,
+                                        help_text=_('A brief description of '
+                                                    'the page. '
+                                                    'Max 255 characters.'),
                                         max_length=255)
+
+    class Meta:
+        verbose_name = _('page')
+        verbose_name_plural = _('pages')
 
     def __unicode__(self):
         return self.title
