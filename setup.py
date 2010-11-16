c5d1817334ddd980af04e16198e78d9dd8e72ed5
Daniele Tricoli <eriol@mornie.org>
2010-11-16 10:34:37 +0100

    Using setuptools

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 from microcms import VERSION
 
 setup(
     name='microcms',
     version=VERSION,
-    description='Simple flatpage enhancement',
+    description='Simple flatpage enhancement.',
     author='Daniele Tricoli',
     author_email='eriol@mornie.org',
-    package_dir={'microcms': 'microcms'},
-    packages=['microcms', 'microcms.templatetags']
+    packages=find_packages(),
+    package_data = {
+        'microcms': [
+            'locale/*/LC_MESSAGES/*',
+        ],
+    },
 )
